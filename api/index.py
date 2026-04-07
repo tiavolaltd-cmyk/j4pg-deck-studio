afc0e04cfcadd689ade3169f74fc082ea6923eb8
tiavolaltd-cmyk <tiavola.ltd@gmail.com>
2026-04-07 12:58:48 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the api directory to the path so we can import generate
+sys.path.insert(0, os.path.dirname(__file__))
+
 from generate import handler
-   
-   __all__ = ['handler']
+
+__all__ = ['handler']
